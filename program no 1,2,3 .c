2ddf1660522a188ffa3d5fa53915d7d2190b772a
Sushant-dhakl <sushantdhakal98@gmail.com>
2024-11-18 17:36:36 +0545

    Add files via upload

---
+++ program no 1,2,3 .c
@@ -0,0 +1,64 @@
+/*#include <stdio.h>                //greater number
+void great(int,int);
+int main()
+{
+    int a,b;
+    printf("enter the two numbers :");
+    scanf("%d%d",&a,&b);
+    great(a,b);
+
+}
+ void great(int a,int b)
+ {
+    if(a>b)
+    printf("%d is the greater number",a);
+    else
+    printf("%d is the greater number",b);
+ }*/
+
+//factorial
+
+ /*#include <stdio.h>
+ void fact(int);
+ int main()
+ {
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    fact(a);
+ }
+  void fact(int a)
+ {
+    int f=1,i;
+    for(i=a;i>=1;i--)
+    {
+        f=f*i;
+    }
+    printf("the factorial  is %d",f);
+    
+ }*/
+
+
+                        //multiplication table
+ /*#include <stdio.h>
+ void multi(int);
+ int main()
+ {
+    int n ;
+    printf("enter the number that is to have multiplied ");
+    scanf("%d",&n);
+    multi(n);
+
+ }
+ void multi(int n)
+ {
+    int i;
+    int m;
+    printf("the multiplication table is \n");
+    for(i=1;i<=10;i++)
+    {
+        m=i*n;
+        printf("%d * %d = %d \n",n,i,m);
+    }
+ }
+*/
