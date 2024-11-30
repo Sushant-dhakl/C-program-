a4fa8275e0aa65976f3e2524af268263b7f44836
Sushant Dhakal -----(motas) <sushantdhakal98@gmail.com>
2024-11-30 11:02:10 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let a=5;
+let b=6;
+console.log("a+b=",a+b);
+num_a = window.prompt("enter 1st number ");
+num_b = window.prompt("enter 2nd number" );
+document.write("the sum of two number is a+b= ",num_a+num_b);
